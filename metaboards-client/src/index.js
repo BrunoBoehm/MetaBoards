09102f041ef58a106c550e33c01b37f71ae99827
BrunoBoehm <bruno.boehm@gmail.com>
2017-07-22 18:56:32 +0200

    index.js now has semantic integration

--- metaboards-client/src/index.js
+++ metaboards-client/src/index.js
@@ -5,5 +5,7 @@ import './semantic/dist/semantic.min.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <App />, document.getElementById('root')
+    );
 registerServiceWorker();
