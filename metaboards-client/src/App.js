b8a0f86dd41e1cc20d2de4ca7e54daefc5465806
BrunoBoehm <bruno.boehm@gmail.com>
2017-07-22 18:55:23 +0200

    Added a header and organized cards as columns

--- metaboards-client/src/App.js
+++ metaboards-client/src/App.js
@@ -1,6 +1,6 @@
 import React, { Component } from 'react';
 import './App.css';
-import { Card, Icon, Image } from 'semantic-ui-react'
+import { Card, Icon, Image, Grid, Header } from 'semantic-ui-react'
 
 
 const boards_data = [
@@ -15,16 +15,45 @@ const boards_data = [
     "intro" : "Does #VanLife suck?",
     "main" : "The main content of the board with long text again",
     "img" : "https://images.follownews.com/1629/1629219/vagabond-mitch-cox-talks-instagram-van-life-and-the-quest-for-an-endless-summer_1.jpg"
+  }, 
+  {
+    "title" : "Surf Life",
+    "intro" : "We love surfing Hawaii",
+    "main" : "The main content of the board with long text again",
+    "img" : "https://cdn.shopify.com/s/files/1/0759/5877/files/Laura_Enever_grande.png?10934993563231938985"
+  },
+  {
+    "title" : "Rock'n'Roll",
+    "intro" : "War on drugs",
+    "main" : "The main content of the board with long text again",
+    "img" : "http://s3.amazonaws.com/medias.photodeck.com/4cf7f48a-a132-41cb-8e92-665637bb2221/HR-TheWarOnDrugs-TBDfest-4Oct2014-9280_xgaplus.jpg"
   }  
 ] 
 
-// const Board = ({ title, intro, main, img }) =>
-//   <section id={title.toLowerCase().replace(/ /g, "-")}>
-//     <img src={img} alt="" />
-//     <h2>{title}</h2>
-//     <h3>{intro}</h3>
-//     <p>{main}</p>
-//   </section>
+const PageHeader = ({title, subtitle}) => (
+  <div className="ui inverted vertical masthead center aligned segment">
+    <div className="ui container">
+      <div className="ui large secondary inverted pointing menu">
+        <a className="toc item">
+          <i className="sidebar icon"></i>
+        </a>
+        <a className="active item">Home</a>
+        <a className="item">Menu 1</a>
+        <a className="item">Menu 2</a>
+        <div className="right item">
+          <a className="ui inverted button">Log in</a>
+          <a className="ui inverted button">Sign Up</a>
+        </div>
+      </div>
+    </div>
+
+    <div className="ui text container">
+      <h1 className="ui inverted header">{title}</h1>
+      <h2>{subtitle}</h2>
+      <div className="ui huge primary button">Get Started <i className="right arrow icon"></i></div>
+    </div>
+  </div>
+)
 
 const Board = ({ title, intro, main, img }) => (
   <Card>
@@ -51,32 +80,47 @@ const Board = ({ title, intro, main, img }) => (
   </Card>
 )
 
-const ListBoards = ({ category, boards }) => 
-  <article>
-    <h2>{category}</h2>
-    <div className="boards">
-      {
-        boards.map((board, i) =>
-          <Board key={i} {...board} />
-        )
-      }
-    </div>
-  </article>
+const Boards = ({ boards }) => 
+  <div className="ui vertical stripe segment">
+    <div className="ui container">
+      <Grid columns={3}>
+        {boards.map((board, i) =>
+          <Grid.Column>
+            <Board key={i} {...board} />
+          </Grid.Column>  
+        )}  
+      </Grid> 
+    </div> 
+  </div>
 
-const Menu = ({title}) =>
-  <header>
-    <h1>{title}</h1>
-  </header>    
+const HeaderSection = ({section_title, title, intro}) => (
+  <section className="ui text container center aligned">
+    <Header as="h3">{section_title}</Header>
+    <Header as="h1">{title}</Header>
+    <p>{intro}</p>
+  </section>
+) 
 
 class App extends Component {
   render() {
     return (
       <div className="App">
-        <Menu title="Cool Stuff" />
-        <ListBoards category="On The Road"  boards={boards_data}/> 
+        <PageHeader title="There's some chocolate left!" subtitle="And don't worry... It always comes with bananas" />
+        <HeaderSection 
+          section_title="INTRO" 
+          title="Welcome to MetaBoards" 
+          intro="That's the intro text that can be quite long already... and introduces the boards to come." 
+        />
+        <Boards boards={boards_data}/> 
+        <HeaderSection 
+          section_title="RESOURCES" 
+          title="That's a new test section" 
+          intro="Another intro text." 
+        />
       </div>
     );
   }
 }
 
 export default App;
+
